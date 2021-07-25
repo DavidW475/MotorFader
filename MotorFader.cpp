f5a660d4efaf39fb42b194f6efed2b70408c6f7f
David Welz <davi.welz98@gmail.com>
2021-07-25 19:09:58 +0200

    Documentation

--- MotorFader.cpp
+++ MotorFader.cpp
@@ -1,5 +1,20 @@
 #include "MotorFader.h"
 
+/**
+ * @param motor_pin_a first motor pin on the Arduino
+ * @param motor_pinb second pin on the Arduino
+ * @param enable_pin enable pin on the Arduino
+ * @param potentiometer_pin potentiometer_pin on the Arduino
+ * @param faderMin the minimum value of the potentiometer
+ * @param faderMax the maximum value of the potentiometer
+ * @param tolerance the tolerance of the target point (potentiometer value)
+ * @param faderSpeed1 fastest fader speed (between 0 and 255)
+ * @param faderSpeed2 medium fader speed (between 0 and 255)
+ * @param faderSpeed3 low fader speed (between 0 and 255)
+ * @param faderSpeedDist1 the potentiometer distance to the target where the fader switches between faderSpeed1 and faderSpeed2
+ * @param faderSpeedDist2 the potentiometer distance to the target where the fader switches between faderSpeed2 and faderSpeed3
+**/
+
 MotorFader::MotorFader(int motor_pin_a, int motor_pin_b, int enable_pin, int potentiometer_pin, int faderMin, int faderMax, int tolerance, int faderSpeed1, int faderSpeed2, int faderSpeed3, int faderSpeedDist1, int faderSpeedDist2){
   this->motor_pin_a = motor_pin_a;
   this->motor_pin_b = motor_pin_b;
@@ -15,34 +30,57 @@ MotorFader::MotorFader(int motor_pin_a, int motor_pin_b, int enable_pin, int pot
   this->faderSpeedDist[1] = faderSpeed2;
 }
 
+/**
+ * @return the minimum value of the potentiometer (faderMin)
+**/
 int MotorFader::GetFaderMin(){
   return faderMin;
 }
 
+/**
+ * @return the maximum value of the potentiometer (faderMax)
+**/
 int MotorFader::GetFaderMax(){
   return faderMax;
 }
 
+/**
+ * @return the tolerance the fader is trying to aim (tolerance)
+**/
 int MotorFader::GetFaderTolerance(){
   return tolerance;
 }
 
+/**
+ * @param target the target of the potentiometer (must be between faderMin and faderMax)
+ * @return the minimum potentiometer value of the target (target-tolerance)
+**/
 int MotorFader::GetFaderMinTarget(int target){
   if (target < faderMin) return faderMin;
   if (target > faderMax) return faderMax;
   return target - tolerance;
 }
 
+/**
+ * @param target the target of the potentiometer (must be between faderMin and faderMax)
+ * @return the maximum potentiometer value of the target (target+tolerance)
+**/
 int MotorFader::GetFaderMaxTarget(int target){
   if (target < faderMin) return faderMin;
   if (target > faderMax) return faderMax;
   return target + tolerance;
 }
 
+/**
+ * @return the current potentiometer value
+**/
 int MotorFader::GetPotentiometerValue(){
   return analogRead(potentiometer_pin);
 }
 
+/**
+ * @return the current pos in percantage between faderMin and faderMax
+**/
 int MotorFader::GetPos(){
   int pos = map(analogRead(potentiometer_pin), faderMin, faderMax, 0, 100);
   if (pos > 100) return 100;
@@ -50,14 +88,24 @@ int MotorFader::GetPos(){
   return pos;
 }
 
+/**
+ * @return the target in percantage
+**/
 int MotorFader::GetTarget(int target){
   return map(target, 0, 100, faderMin, faderMax);
 }
 
+/**
+ * @param val the new fader speed (between 255 and 0)
+**/
 void MotorFader::SetFaderSpeed(int val){
   analogWrite(enable_pin, faderSpeed[val]);
 }
 
+/**
+ * moves the fader to a target position
+ * @param target the target the fader moves to (values between 0 and 100)
+**/
 void MotorFader::ToPos(int target){
   if (target > 100 || target < 0) return;
   int currentPos = GetPotentiometerValue();
@@ -97,16 +145,25 @@ void MotorFader::ToPos(int target){
   MotorStop();
 }
 
+/**
+ * fader moves up
+**/
 void MotorFader::MotorUp(){
   digitalWrite(motor_pin_a, HIGH);
   digitalWrite(motor_pin_b, LOW);
 }
 
+/**
+ * fader moves down
+**/
 void MotorFader::MotorDown(){
   digitalWrite(motor_pin_a, LOW);
   digitalWrite(motor_pin_b, HIGH);
 }
 
+/**
+ * fader stops
+**/
 void MotorFader::MotorStop(){
   digitalWrite(motor_pin_a, LOW);
   digitalWrite(motor_pin_b, LOW);
